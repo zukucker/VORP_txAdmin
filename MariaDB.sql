63f690707d1156c1681fe3362b92d0a4f81dd73b
zukucker <alexander.froehling@googlemail.com>
2023-05-30 14:21:30 +0200

    try: fix the sql error

--- MariaDB.sql
+++ MariaDB.sql
@@ -104,7 +104,7 @@ CREATE TABLE IF NOT EXISTS `characters` (
   KEY `compPlayer` (`compPlayer`(768)),
   KEY `info` (`info`(768)),
   KEY `inventory` (`inventory`(768)),
-  KEY `coords` (`coords`),
+  KEY `coords` (`coords`(768)),
   KEY `money` (`money`),
   KEY `meta` (`meta`),
   KEY `steamname` (`steamname`),
